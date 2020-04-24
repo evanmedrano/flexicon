612c3ef55af4ee7d230427cd96e8d66e2994e28a
Evan Medrano <evan.a.medrano@gmail.com>
2020-04-24 06:46:16 -0400

    Add header with night and light mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 
-function Header() {
+function Header(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  const currentPageColor = props.pageColor === 'light' ? 'Night mode' : 'Light mode'
+
+  const handlePageChange = () => {
+    props.currentPageColor(currentPageColor)
+  }
+
+
   return (
     <>
      <header className="header">
-       <nav className="header__nav">
-         <ul className="header__list">
-           <li className="header__item">
-             <Link to="/about">About</Link>
-           </li>
-           <li className="header__item">
-             <Link to="/">Home</Link>
-           </li>
-         </ul>
-       </nav>
+       <Navbar
+         color={props.pageColor}
+         dark={!props.isLight}
+         light={props.isLight}
+         expand="md"
+       >
+         <Container>
+           <NavbarBrand tag={Link} to={"/"}>life doesn't suck</NavbarBrand>
+           <NavbarToggler onClick={toggle} />
+           <Collapse isOpen={isOpen} navbar>
+             <Nav className="mr-auto" navbar>
+               <NavItem>
+                 <NavLink onClick={handlePageChange}>{currentPageColor}</NavLink>
+               </NavItem>
+               <NavItem>
+                 <NavLink tag={Link} to={"/blog"}>Blog</NavLink>
+               </NavItem>
+               <NavItem>
+                 <NavLink tag={Link} to={"/motivation"}>Motivation</NavLink>
+               </NavItem>
+               <UncontrolledDropdown nav inNavbar>
+                 <DropdownToggle nav caret>
+                   Options
+                 </DropdownToggle>
+                 <DropdownMenu right>
+                   <DropdownItem>
+                     Option 1
+                   </DropdownItem>
+                   <DropdownItem>
+                     Option 2
+                   </DropdownItem>
+                   <DropdownItem>
+                     Option 3
+                   </DropdownItem>
+                   <DropdownItem divider />
+                   <DropdownItem>
+                     Reset
+                   </DropdownItem>
+                 </DropdownMenu>
+               </UncontrolledDropdown>
+             </Nav>
+             <NavbarText tag={Link} to={"/login"}>Log in</NavbarText>
+           </Collapse>
+        </Container>
+       </Navbar>
      </header>
     </>
   )
--- src/root/App.js
+++ src/root/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/index.scss';
 import Routes from '../routes';
 import { Header } from '../components';
 
 function App() {
+  const [isLight, setIsLight] = useState(true);
+  const [pageColor, setPageColor] = useState('light');
+
+  const handlePageChange = (change) => {
+    if (change === 'Night mode') {
+      setPageColor('dark')
+      setIsLight(false)
+    } else {
+      setPageColor('light');
+      setIsLight(true)
+    }
+  }
+
+  const backgroundColor = pageColor === 'light' ? 'hsl(210,17%,98%)' : 'hsl(210,10%,23%)'
+  const textColor = pageColor === 'light' ? 'hsl(180,0%,43%)' : 'hsl(0,0%,100%)'
+
+  const myStyle = {
+    backgroundColor: `${backgroundColor}`,
+    color: `${textColor}`,
+    height: "100vh"
+  }
+
   return (
-    <Routes
-      header={<Header />}
-    />
+    <div style={myStyle}>
+      <Routes
+        header={
+          <Header
+            currentPageColor={handlePageChange}
+            isLight={isLight}
+            pageColor={pageColor}
+          />
+        }
+      />
+    </div>
   );
 }
 
