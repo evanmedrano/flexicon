234ec2dda9f6a4a9fcf1907c4a3bb079ecabb279
Evan Medrano <evan.a.medrano@gmail.com>
2020-04-30 19:45:19 -0400

    Add functionality for user signup

--- src/screens/Signup/Signup.js
+++ src/screens/Signup/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,30 @@ import { Link } from "react-router-dom";
 import { Container } from "reactstrap";
 
 function Signup() {
+  const [form, setState] = useState({
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  })
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    axios.post('http://localhost:3030/api/v1/signup', {
+      user: {
+        email: form.email,
+        password: form.password,
+        password_confirmation: form.passwordConfirmation
+      }
+    })
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+
+  const handleInputChange = (event) => {
+    setState({...form, [event.target.name]: event.target.value })
+  }
+
   return (
     <Container>
       <div className="form form--shadow">
@@ -27,41 +51,43 @@ function Signup() {
           <span>Sign up with Google</span>
         </Link>
 
-        <form autocomplete="on">
-          <div className="form__field">
-            <label for="username" className="form__label">
-              Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              autofocus
-              className="form__input"
-            />
-          </div>
-
+        <form autocomplete="on" onSubmit={handleFormSubmit}>
           <div className="form__field">
             <label for="email" className="form__label">
               Email
             </label>
-            <input type="email" name="email" className="form__input" />
+            <input
+              type="email"
+              name="email"
+              className="form__input"
+              onChange={handleInputChange}
+              value={form.email}
+            />
           </div>
 
           <div class="form__field">
             <label for="password" className="form__label">
               Password
             </label>
-            <input type="password" name="password" className="form__input" />
+            <input
+              type="password"
+              name="password"
+              className="form__input"
+              onChange={handleInputChange}
+              value={form.password}
+            />
           </div>
 
           <div class="form__field">
-            <label for="password-confirmation" className="form__label">
+            <label for="passwordConfirmation" className="form__label">
               Password Confirmation
             </label>
             <input
               type="password"
-              name="password-confirmation"
+              name="passwordConfirmation"
               className="form__input"
+              onChange={handleInputChange}
+              value={form.passwordConfirmation}
             />
           </div>
 
